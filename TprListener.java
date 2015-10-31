c69050182a601956dedaa9495bea68277bd7c84e
indosandi <indosandi@gmail.com>
2015-10-31 13:40:52 -0500

    remove outside files and add Main

--- TprListener.java
+++
@@ -1,7 +0,0 @@
-import java.awt.image.BufferedImage; 
-interface TprListener{
-    public void typeListener(int i, boolean x); 
-    public void imageListener(BufferedImage b, int i); 
-    public void eraseListener(); 
-    public void posListener(int[] x,int[] y); 
-}
